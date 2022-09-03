adadbe73b9cb6904bab0a876430a087c25b11829
Toskyll <jans.guillope@gmail.com>
2022-09-03 08:46:09 +0200

    adding stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a href="https://www.youtube.com/watch?v=eBGIQ7ZuuiU">Clique</a>
       </header>
     </div>
   );
